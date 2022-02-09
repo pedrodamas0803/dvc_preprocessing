b732059d2839c5109948c2372d1c03d9579e89d4
Pedro <pdrodresende@gmail.com>
2022-02-09 16:53:13 +0100

    hoping fot the best

---
+++ __init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from dvc_preprocessing import preprocessing, plot
+
+
+__version__ = "0.0.1"
--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 from setuptools import setup, find_packages
 
 setup(name="dvc_preprocessing",
       version="0.0.1",
+      url = "https://github.com/pedrodamas0803/dvc_preprocessing",
       description="A small library to avoid human interaction in data preprocessing for DVC. (but it was written by a not very clever human)",
       author="Pedro Damas Resende",
       packages=find_packages()
