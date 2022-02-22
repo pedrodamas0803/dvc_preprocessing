6cb679ec98cfb1669405189aebc0783289361c8a
Pedro D. Resende <pedro.damas_resende@esrf.fr / pdrodresende@gmail.com>
2022-02-22 12:02:05 +0100

    minor changes in __init__

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from dvc_preprocessing import preprocessing, plot
+from dvc_preprocessing import preprocessing, plot, macros, constants
+from tests import test
 
 
 __version__ = "0.0.1"
